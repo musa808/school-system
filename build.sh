eebbe0799faae429b5ea8e675a8284ec65c5a23c
musa808 <mohamudmusa115@gmail.com>
2026-05-05 19:50:36 +0800

    Debug superuser creation

--- build.sh
+++ build.sh
@@ -9,14 +9,12 @@ python manage.py collectstatic --noinput
 
 python manage.py shell -c "
 from school.models import User
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@example.com', 'Admin1234!')
-    print('Superuser created')
-else:
-    u = User.objects.get(username='admin')
-    u.set_password('Admin1234!')
-    u.is_superuser = True
-    u.is_staff = True
-    u.save()
-    print('Superuser password reset')
+print('All users:', list(User.objects.values('username', 'is_superuser', 'is_staff', 'is_active')))
+u, created = User.objects.get_or_create(username='admin')
+u.set_password('Admin1234!')
+u.is_superuser = True
+u.is_staff = True
+u.is_active = True
+u.save()
+print('Done. Created:', created)
 "
